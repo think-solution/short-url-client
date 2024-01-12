04159b922a864cd74ab9794bb7aeea2fa2c5b9c6
Jana Sivaraman <jana.sivaraman@entrust.com>
2024-01-12 00:41:40 -0500

    Homepage initial; Processor initial

--- small-URL/src/app/app.module.ts
+++ small-URL/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AccountComponent } from './account/account.component';
 import { ProcessorComponent } from './processor/processor.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,13 @@ import { ProcessorComponent } from './processor/processor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- small-URL/src/app/processor/processor.component.ts
+++ small-URL/src/app/processor/processor.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogModule, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface DialogData {
+  errorMsg: string;
+}
 
 @Component({
   selector: 'app-processor',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProcessorComponent implements OnInit {
 
-  constructor() { }
+  url = 'Enter your URL and hit GO!';
+  errorMsg = '';
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  shortenUrl(){
+    if(this.url === ''){
+      this.errorMsg='Please enter a URL to continue.'
+      this.dialog.open(ErrorDialog,{data:{errorMsg: this.errorMsg}});
+    }
+  }
+
+  
+}
+
+@Component({
+  selector: 'error-dialog',
+  templateUrl: 'processor.errorMsg.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class ErrorDialog {
+  constructor(public dialogRef: MatDialogRef<ErrorDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData){
+    }
+
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
 }
