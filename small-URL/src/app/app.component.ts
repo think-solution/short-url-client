643c2b832522e5811c722354b64a44494386a5fe
Jana S <jansub93@gmail.com>
2024-02-07 23:04:39 -0500

    test8

--- small-URL/src/app/app.component.ts
+++ small-URL/src/app/app.component.ts
@@ -11,27 +11,26 @@ import { URLDataService } from './services/urldata.service';
 })
 export class AppComponent {
   title = 'small-URL';
-  displayContent : boolean = true;
+  displayContent : boolean = false;
 
 constructor(private router : Router, private urlDataService : URLDataService, private processUrlService : ProcessURLService) {
   let shortCode = localStorage.getItem('shortCode');
+  this.displayContent = localStorage.getItem('displayContent') === 'true';
   if(shortCode){
-    this.displayContent = false;
     urlDataService.getUrlDetailsSimple(shortCode).then((data) => {
       if(data){
         localStorage.removeItem('shortCode');
         processUrlService.redirect(shortCode);
       } else {
         console.error('Could not find the path specified.');
-        localStorage.removeItem('shortCode');
-        window.location.href = URL_CONSTANTS.kutieURLBase;
       }
     }).catch((e) => {
       console.error('Could not find the path specified.');
-      localStorage.removeItem('shortCode');
-      window.location.href = URL_CONSTANTS.kutieURLBase;
-    });
+      });
     }
+    localStorage.removeItem('shortCode');
+    localStorage.setItem('displayContent', 'true');
+    window.location.href = URL_CONSTANTS.kutieURLBase;
   }
   
 }
