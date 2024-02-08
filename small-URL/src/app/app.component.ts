4b68e537c70611963f8f67625f4051720115705e
Jana S <jansub93@gmail.com>
2024-02-07 20:34:20 -0500

    test3

--- small-URL/src/app/app.component.ts
+++ small-URL/src/app/app.component.ts
@@ -13,27 +13,27 @@ export class AppComponent {
   title = 'small-URL';
 
   constructor(private router : Router, private urlDataService : URLDataService) {
-    let path = window.location.href;
-    let routeArr = URL_CONSTANTS.routes.split(',');
-    if(!(path === URL_CONSTANTS.kutieURLBase || path === URL_CONSTANTS.kutieURLBase + '/')){ //If absolute path, continue
-      if(path.startsWith(URL_CONSTANTS.kutieURLBase)){
-        let shortCode = path.substring(50); //Might need updating once domain is confirmed for production.
-        if(routeArr.includes(shortCode)){
-          //Internal rountes, do nothing.
-        } else {
-          urlDataService.getUrlDetailsSimple(shortCode).then((data) => {
-            if(data){
-              //Correct shortCode, do nothing. Backend will handle any redirection.
-            } else {
-              console.error('Could not find the path specified.');
-              window.location.href = URL_CONSTANTS.kutieURLBase;
-            }
-          }).catch((e) => {
-            console.error('Could not find the path specified.');
-            window.location.href = URL_CONSTANTS.kutieURLBase;
-          });
-        }
-      }
-    }
+    // let path = window.location.href;
+    // let routeArr = URL_CONSTANTS.routes.split(',');
+    // if(!(path === URL_CONSTANTS.kutieURLBase || path === URL_CONSTANTS.kutieURLBase + '/')){ //If absolute path, continue
+    //   if(path.startsWith(URL_CONSTANTS.kutieURLBase)){
+    //     let shortCode = path.substring(50); //Might need updating once domain is confirmed for production.
+    //     if(routeArr.includes(shortCode)){
+    //       //Internal rountes, do nothing.
+    //     } else {
+    //       urlDataService.getUrlDetailsSimple(shortCode).then((data) => {
+    //         if(data){
+    //           //Correct shortCode, do nothing. Backend will handle any redirection.
+    //         } else {
+    //           console.error('Could not find the path specified.');
+    //           window.location.href = URL_CONSTANTS.kutieURLBase;
+    //         }
+    //       }).catch((e) => {
+    //         console.error('Could not find the path specified.');
+    //         window.location.href = URL_CONSTANTS.kutieURLBase;
+    //       });
+    //     }
+    //   }
+    // }
   }
 }
